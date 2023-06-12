4253d6b3f9aded83514984a99d1d1808528a5c87
ttndev2 <ttndev2@gmail.com>
2023-06-13 02:46:16 +0700

    fix test

--- src/recipes/recipes.controller.spec.ts
+++ src/recipes/recipes.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecipesController } from './recipes.controller';
+import { RecipesService } from './recipes.service';
+import { PrismaService } from '../prisma.service';
 
 describe('RecipesController', () => {
   let controller: RecipesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService, RecipesService],
       controllers: [RecipesController],
     }).compile();
 
