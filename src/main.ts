f45f8c0292fe65cb438a7f2c67653eb1f27514cf
ttndev2 <ttndev2@gmail.com>
2023-06-13 02:14:13 +0700

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000; // Fall back to port 3000 if no environment variable is set
+  await app.listen(port);
 }
 bootstrap();
